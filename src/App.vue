c4bc78a2efc35be2edbf8166fedc127085b95ab6
huxingya <2977705720@qq.com>
2019-05-08 16:55:27 +0800

    第4次提交

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,30 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
-<style scoped="">
-
+<style>
+  html,body{
+    height: 100%;
+    width: 100%;
+    background: #eee;
+    color: #101010;
+    font-size: 16px;
+  }
+  body,p{
+    margin: 0;
+    padding: 0;
+  }
+  img{
+    display: block;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 </style>
